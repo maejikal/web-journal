cd6f7fffee5d993bf22d2be0cc6e1beae6a9006c
maejikal <69302838+maejikal@users.noreply.github.com>
2024-03-04 13:44:49 +0700

    Update script.js
    more validation/display features

--- prototype/static/script.js
+++ prototype/static/script.js
@@ -13,3 +13,53 @@ function showVerses() {
   const extra = document.getElementsByClassName('.extra');
   extra.classList.toggle('extra-show');
 }
+
+function validate() {
+  pass1 = document.getElementById("password").value
+  pass2 = document.getElementById("confirm_password").value
+  bt = document.getElementById("submit-btn")
+  if (pass1 != pass2) {
+    bt.disabled = true;
+    document.getElementById("validate-status").textContent = "passwords don't match!";
+    document.getElementById("validate-status").style.color = 'red';
+  } else {
+    bt.disabled = false;
+    document.getElementById("validate-status").textContent = "passwords match!";
+    document.getElementById("validate-status").style.color = 'green';
+  }
+}
+
+function showEdit() {
+  var modal = document.getElementById("options-div");
+
+  if (modal.style.display === "block") {
+    modal.style.display = "none";
+  } else {
+    modal.style.display = "block";
+  }
+}
+
+function showSave() {
+  const save = document.getElementById('save_form');
+  if (save.style.display === "block") {
+    save.style.display = "none";
+  } else {
+    save.style.display = "block";
+  }
+}
+
+function confirm() {
+  const save = document.getElementById('confirm');
+  if (save.style.display === "block") {
+    save.style.display = "none";
+  } else {
+    save.style.display = "block";
+  }
+}
+
+//new feature to be added: toggle between dark and light mode
+function switchMode() {
+  var el = document.body;
+  el.classList.toggle('light-mode');
+}
+
